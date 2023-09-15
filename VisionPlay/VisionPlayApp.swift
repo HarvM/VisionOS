80dd0004a529d492058dbf1da639bf5c4c691256
Marc <marc_harvey@flutterint.com>
2023-09-15 13:45:17 +0100

    Initial Commit

---
+++ VisionPlay/VisionPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionPlayApp.swift
+//  VisionPlay
+//
+//  Created by Marc Harvey on 15/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct VisionPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
